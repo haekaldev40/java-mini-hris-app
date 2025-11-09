294ea984677e3a34fecfe86e2af8e419ddf88100
Qicepppppp <qicepppppp@MacBook-Air-Qicepppppp-3976.local>
2025-11-09 13:23:19 +0700

    Initial commit : setup project dependencies

---
+++ src/main/java/minihris/example/mini_hris_project/MiniHrisProjectApplication.java
@@ -0,0 +1,13 @@
+package minihris.example.mini_hris_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHrisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniHrisProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minihris/example/mini_hris_project/MiniHrisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package minihris.example.mini_hris_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniHrisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
